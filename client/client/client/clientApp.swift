5b5f8e6bd5eab5b5d8812d6818ffa77e820818f8
tomohirojin <tomohiro_jin@mail.toyota.co.jp>
2023-12-20 17:33:39 +0900

    add: first-client commit

---
+++ client/client/client/clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  clientApp.swift
+//  client
+//
+//  Created by user on 2023/12/20.
+//
+
+import SwiftUI
+
+@main
+struct clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
